6c19e3468d88bf2f9a092d0c7e2ef148c0b6f3f0
zanutt <matszg@live.com>
2024-12-11 14:01:43 -0300

    fix exposing credentials vulnerability

--- infra/database.js
+++ infra/database.js
@@ -10,14 +10,6 @@ async function query(queryObject) {
     ssl: process.env.NODE_ENV === 'development' ? false : true,
   });
 
-  console.log(" Credenciais do postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  })
-
 
   let result;
 
